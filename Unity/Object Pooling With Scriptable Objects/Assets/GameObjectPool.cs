361babb2f5f3605941a6bbbefb67b4d38f6b4da3
Collin <genaralskar1@gmail.com>
2020-01-23 14:47:52 -0700

    add object pooling stuff

---
+++ Unity/Object Pooling With Scriptable Objects/Assets/GameObjectPool.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Pools/Game Object Pool")]
+public class GameObjectPool : ScriptableObject
+{
+    public GameObject prefab;
+
+    public int amount;
+    
+    private Queue<GameObject> spawnedObjs;
+
+    private Transform parent;
+
+    public void SpawnPool()
+    {
+        if (spawnedObjs == null || spawnedObjs.Count == 0)
+        {
+            spawnedObjs = new Queue<GameObject>();
+        }
+
+        if (spawnedObjs.Count >= amount)
+        {
+            return;
+        }
+
+        if (!parent)
+        {
+            parent = new GameObject(name).transform;
+        }
+
+        while (spawnedObjs.Count < amount)
+        {
+            GameObject obj = Instantiate(prefab, parent);
+            obj.SetActive(false);
+            spawnedObjs.Enqueue(obj);
+        }
+    }
+
+    public GameObject GetPooledObject(Vector3 newPos, Quaternion newRot)
+    {
+        if (spawnedObjs == null || spawnedObjs.Count == 0)
+        {
+            SpawnPool();
+            Debug.LogWarning($"{name} spawned mid-game. Consider spawning it at the start of the game");
+        }
+
+        GameObject obj = spawnedObjs.Dequeue();
+        
+        spawnedObjs.Enqueue(obj);
+        obj.SetActive(false);
+        
+        obj.transform.position = newPos;
+        obj.transform.rotation = newRot;
+        obj.SetActive(true);
+
+        return obj;
+    }
+}
---
+++ Unity/Object Pooling With Scriptable Objects/Assets/PoolFountain.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolFountain : MonoBehaviour
+{
+    public GameObjectPool pool;
+    
+    public float spawnTime = .1f;
+
+    public float force = 50;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        while (true)
+        {
+            GameObject obj = pool.GetPooledObject(transform.position, transform.rotation);
+            Rigidbody rb = obj.GetComponent<Rigidbody>();
+            if (rb)
+            {
+                rb.AddForce(transform.up * force);
+            }
+            
+            yield return new WaitForSeconds(spawnTime);
+        }
+    }
+}
